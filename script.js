ebcff36bdb2c79e208dc70966c40144b2bbbf779
Senem Soy <ssoy23@gmail.com>
2017-12-27 21:39:26 -0800

    Random Quote Generator first version

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	function getNewQuote(){
+
+		var quote;
+		var author; 
+
+		$.ajax({
+			url: 'http://api.forismatic.com/api/1.0/',
+			jsonp: 'jsonp',
+			dataType: 'jsonp',
+			data: {
+				method: 'getQuote',
+				lang: 'en',
+				format: 'jsonp'
+			},
+			success: function(response) {
+				quote = response.quoteText;
+				author = response.quoteAuthor;
+				$('#quote').text(quote);
+				if (author) {
+					$('#author').text('said by ' + author);
+				}
+				else{
+					$('#author').text('- unknown');
+				}
+			}
+			});
+	}
+
+getNewQuote();
+
+$('.get-quote').on('click', function(event){
+	event.preventDefault();
+	getNewQuote();
+})
+
+$('.share-quote').on('click', function(even){
+	event.preventDefault();
+	window.open('https://twitter.com/intent/tweet?text='+encodeURIComponent(quote.innerHTML + ' --' +author.innerHTML));
+})
+
+});
